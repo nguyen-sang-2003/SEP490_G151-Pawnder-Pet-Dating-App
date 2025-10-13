cc96f0e1462b0872ed746b06989736a2e98f97a3
Nguyễn Minh Sáng <sangnmhe176048@fpt.edu.vn>
2025-10-13 14:52:28 +0700

    Add Login + Logout (Encode password + validate email, password)
    - Add endpoint Login + Logout
    - Encode password SHA256
    - validate email, password by Data Annotations

---
+++ BE/BE/Controlers/AuthController.cs
@@ -0,0 +1,91 @@
+﻿using BE.Models;
+using BE.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Security.Claims;
+
+namespace BE.Controlers
+{
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly PawnderDatabaseContext _context;
+        private readonly PasswordService _passwordService;
+        private readonly TokenService _tokenService;
+        public AuthController(PawnderDatabaseContext context, TokenService tokenService)
+        {
+            _context = context;
+            _passwordService = new PasswordService();
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginRequest request)
+        {
+            var user = _context.Users.Include(u => u.Role).FirstOrDefault(u => u.Email == request.Email);
+            if (user == null)
+            {
+                return Unauthorized("Tài khoản không tồn tại");
+            }
+
+            bool isPasswordValid = _passwordService.VerifyPassword(request.Password, user.Passwordhash);
+
+            if (!isPasswordValid)
+                return Unauthorized("Sai mật khẩu");
+            
+            var token = _tokenService.GenerateToken(user.Userid,user.Role.Rolename);
+
+            user.Tokenjwt = token;
+            _context.Users.Update(user);
+            _context.SaveChanges();
+
+            return Ok(new
+            {
+                Message = "Đăng nhập thành công",
+                Token = token
+            });
+        }
+        public class LoginRequest
+        {
+            [Required(ErrorMessage = "Email không được để trống")]
+            [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+            public string Email { get; set; }
+            [Required(ErrorMessage = "Mật khẩu không được để trống")]
+            [StringLength(100, MinimumLength = 6, ErrorMessage = "Mật khẩu phải ít nhất 6 ký tự")]
+            public string Password { get; set; }
+        }
+
+        [Authorize]
+        [HttpPost("logout")]
+        public async Task<ActionResult> Logout()
+        {
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                if (string.IsNullOrEmpty(userId))
+                    return Unauthorized("Không xác định được người dùng.");
+
+                var id = int.Parse(userId);
+
+                var user = _context.Users.FirstOrDefault(u => u.Userid == id);
+                if (user == null)
+                    return NotFound("Không tìm thấy người dùng.");
+
+                user.Tokenjwt = null;
+                _context.Users.Update(user);
+                await _context.SaveChangesAsync();
+
+                return Ok("Đăng xuất thành công.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Lỗi hệ thống: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ BE/BE/Services/PasswordService.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace BE.Services
+{
+    public class PasswordService
+    {
+        public string HashPassword(string password)
+        {
+            using (var sha = SHA256.Create())
+            {
+                var bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var builder = new StringBuilder();
+
+                foreach (var b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+        public bool VerifyPassword(string inputPassword, string hashedPassword)
+        {
+            string hashedInput = HashPassword(inputPassword);
+            return hashedInput == hashedPassword;
+        }
+    }
+}
