1b5d145102630fea9410c1e1f46f2964bc18059f
Nguyễn Minh Sáng <sangnmhe176048@fpt.edu.vn>
2025-12-03 01:41:15 +0700

    update domain

--- FE/FE-User/src/config/api.config.ts
+++ FE/FE-User/src/config/api.config.ts
@@ -6,7 +6,7 @@
 export const API_CONFIG = {
   // Development URLs
   // ANDROID_EMULATOR: 'http://10.0.2.2:5297',
-  ANDROID_EMULATOR: 'https://nonenigmatical-catarrhally-elvie.ngrok-free.dev',
+  ANDROID_EMULATOR: 'https://sep490g151-pawnder-pet-dating-app-production.up.railway.app',
   IOS_SIMULATOR: 'http://localhost:5297',
   
   // Production URL - Azure Backend
